c5d8a11797487ddaba61d62cb067dc1c5479fa0b
ajradtke <100101271+ajradtke@users.noreply.github.com>
2022-07-11 19:06:40 -0400

    exported daily challenge functions to react app

---
+++ dayone-app/src/utils.js
@@ -0,0 +1,15 @@
+const API_URL = 'http://www.omdbapi.com';
+const API_KEY = '';
+
+export const getMoviesByName = async(name='Batman') => {
+  const response = await fetch(`${API_URL}/?apiKey=${API_KEY}&s=${name}`);
+  const data = await response.json();
+
+  console.log('10 first results of movies:', data);
+}
+
+export const getMovieDetailsById = async (movieId='tt3032476') => {
+  const response = await fetch(`${API_URL}/?apiKey=${API_KEY}&i=${movieId}`);
+  const data = await response.json();
+  console.log('Movie:', data);
+}
